6e4c7bb602257b7e18f230c632474d0ce194d4b9
gopiteekenam <>
2019-09-05 16:48:46 +0530

    with configuration

--- src/main/scala/com/example/QuickstartServer.scala
+++ src/main/scala/com/example/QuickstartServer.scala
@@ -8,10 +8,12 @@ import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Sink, Source}
+import com.typesafe.config.ConfigFactory
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.util.{Failure, Success}
+import concurrent.duration._
 
 
 //#main-class
@@ -34,9 +36,12 @@ object QuickstartServer extends App with UserRoutes {
   //#main-class
   val subUri = "/users/mojombo"
 
+  val serviceUrl = {
+    val config = ConfigFactory.load()
+    config.getString("services.git-api.host")
+  }
 
-
-  val httpClient = Http().outgoingConnectionHttps(host = "api.github.com")
+  val httpClient = Http().outgoingConnectionHttps(host = serviceUrl)
   val future: Future[User] =
     Source.single(HttpRequest(uri = Uri(subUri)))
     .via(httpClient)
@@ -44,7 +49,13 @@ object QuickstartServer extends App with UserRoutes {
     .runWith(Sink.head)
 
   future.onComplete {
-    case Success(res) => println(res)
+    case Success(res) =>
+
+      val start = System.currentTimeMillis()
+      val result = Await.result(future, 15 seconds)
+      val end = System.currentTimeMillis()
+      println(res)
+      system.terminate()
     case Failure(e) =>
       Console.err.println(s"Server could not start!")
       e.printStackTrace()
